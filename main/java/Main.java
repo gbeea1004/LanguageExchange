87fe021539100c546f4e7a34856abd0d1befd3d2
geonHee <corqkrtk3@naver.com>
2019-02-03 18:55:32 +0900

    두번째 라임 테스트

--- main/java/Main.java
+++ main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("라임 테스트");
         System.out.printf("포에버 테스트");
+        System.out.println("두번째 라임 테스트");
     }
 }
