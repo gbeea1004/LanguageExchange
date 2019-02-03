30bdce6a43ee341cd882faddaadb42c0e2469bad
TaeSu <lollolzkk@gmail.com>
2019-02-03 19:26:23 +0900

    test2 complete

--- main/java/Main.java
+++ main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.printf("포에버 테스트");
         System.out.println("두번째 라임 테스트");
         System.out.println("개힘드네");
+        System.out.printf("test2");
     }
 }
