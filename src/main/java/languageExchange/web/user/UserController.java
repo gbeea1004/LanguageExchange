745bccaf1f9aa11aaa3652eb4cdc358baf00c056
geonHee <corqkrtk3@naver.com>
2019-02-12 17:24:47 +0900

    main/java 패키지 분리

--- src/main/java/languageExchange/web/user/UserController.java
+++ src/main/java/languageExchange/web/user/UserController.java
@@ -1,4 +1,4 @@
-package languageExchange.domain.web.user;
+package languageExchange.web.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
