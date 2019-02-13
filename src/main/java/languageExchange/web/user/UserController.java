40d9859f062378413842f3ba887602b56ee9ef3a
daesoop <kimdaesoop@hotmail.com>
2019-02-13 14:36:04 +0900

    아이디, 비밀번호, 이름, 나이, 배울언어, 모국어 데이터를 가진 회원가입 완료

--- src/main/java/languageExchange/web/user/UserController.java
+++ src/main/java/languageExchange/web/user/UserController.java
@@ -4,6 +4,7 @@ import languageExchange.domain.User;
 import languageExchange.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,10 @@ public class UserController {
     }
 
     @PostMapping("")
-    public String create(User user) {
+    public String create(User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "redirect:/users/form";
+        }
         userService.add(user);
         return "redirect:/users/login";
     }
