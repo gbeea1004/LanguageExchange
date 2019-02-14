b34d18561b2c87d1e746fd4aac649aba5e07e1e3
geonHee <corqkrtk3@naver.com>
2019-02-14 18:12:35 +0900

    log Test

--- src/main/java/languageExchange/web/user/UserController.java
+++ src/main/java/languageExchange/web/user/UserController.java
@@ -2,6 +2,7 @@ package languageExchange.web.user;
 
 import languageExchange.domain.User;
 import languageExchange.service.UserService;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    private static final Logger log = getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
